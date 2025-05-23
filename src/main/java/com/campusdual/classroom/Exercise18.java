46e171b5f9da50189108aaa92361aedb524f9d39
Pablo <pablobarreiro131@gmail.com>
2025-05-23 09:46:54 +0200

    Ejercicio 18, completado y testeado.

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -4,5 +4,23 @@ public class Exercise18 {
 
 	public static void main(String[] args) {
 
+		showInlineArray(createAndInitializeArray(8));
+	}
+
+	public static int[] createAndInitializeArray(int num) {
+		int[] array = new int[num];
+		for (int i = 0; i<array.length; i++){
+			array[i] = i + 1;
+		}
+		return array;
+	}
+
+	public static void showInlineArray(int[] intArray) {
+		for (int i = 0; i<intArray.length; i++){
+			System.out.print(intArray[i]);
+			if (i<intArray.length-1){
+				System.out.print(" ");
+			}
+		}
 	}
 }
